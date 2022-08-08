03406f64a6dabff951e8c1411507df9900255f91
hsumyatyehtet <hsumyatyehtet@gmail.com>
2022-08-08 07:36:54 -0700

    bottom bar set up

--- app/src/main/java/com/hmyh/toyu/activity/MainActivity.kt
+++ app/src/main/java/com/hmyh/toyu/activity/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.hmyh.toyu.activity
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import androidx.core.content.ContextCompat
 import com.hmyh.toyu.R
 import com.hmyh.toyu.databinding.ActivityMainBinding
 
@@ -16,6 +19,21 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setUpBottomBar()
+    }
+
+    @SuppressLint("WrongConstant")
+    private fun setUpBottomBar() {
+        binding.bottomNavBar.labelVisibilityMode = 1
+        binding.bottomNavBar.background =
+            ContextCompat.getDrawable(this,R.drawable.bg_bottom_bar)
+
+        val dimensionInDp = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, 20.0f,
+            resources.displayMetrics
+        ).toInt()
+        binding.bottomNavBar.itemIconSize = dimensionInDp
     }
 
     companion object{
